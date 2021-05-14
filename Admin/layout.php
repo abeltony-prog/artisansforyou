11ff7a7a6eb354ba89890b68b2053c5b752c9c0c
abeltony-prog <abelstilesnani280@aol.com>
2021-05-13 22:46:10 -0700

    artisan list added

--- Admin/layout.php
+++ Admin/layout.php
@@ -23,38 +23,10 @@
       <div class="top-nav notification-row">
         <!-- notificatoin dropdown start-->
         <ul class="nav pull-right top-menu">
-          <!-- inbox notificatoin start-->
-          <li id="mail_notificatoin_bar" class="dropdown">
-            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                            <i class="icon-envelope-l"></i>
-                        </a>
-            <ul class="dropdown-menu extended inbox">
-              <div class="notify-arrow notify-arrow-blue"></div>
-              <li>
-                <p class="blue">You have 5 new messages</p>
-              </li>
-              <li>
-                <a href="#">
-                                    <span class="photo"><img alt="avatar" src="./img/avatar-mini.jpg"></span>
-                                    <span class="subject">
-                                    <span class="from">Greg  Martin</span>
-                                    </span>
-                                    <span class="message">
-                                        I really like this admin panel.
-                                    </span>
-                                </a>
-              </li>
-              <li>
-                <a href="#">See all messages</a>
-              </li>
-            </ul>
-          </li>
-          <!-- inbox notificatoin end -->
           <!-- user login dropdown start-->
           <li class="dropdown">
             <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                             <span class="profile-ava">
-                                <img alt="" src="img/<?php echo $user['profile'] ?>">
                             </span>
                             <span class="username"><?php echo $user['name'] ?></span>
                             <b class="caret"></b>
@@ -62,7 +34,7 @@
             <ul class="dropdown-menu extended logout">
               <div class="log-arrow-up"></div>
               <li class="eborder-top">
-                <a href="changepassword.php"><i class="icon_profile"></i>Change Password</a>
+                <a href="changepassword.php"><i class="icon_lock"></i>Change Password</a>
               </li>
               <li>
                 <form class="" action="" method="post">
@@ -112,6 +84,23 @@ if (isset($_POST['logout'])) {
         <ul class="sub">
           <li><a class="" href="category_list.php">Category List</a></li>
           <li><a class="" href="addcate.php">Add New</a></li>
+        </ul>
+      </li>
+      <li class="sub-menu">
+        <a href="javascript:;" class="">
+                      <i class="fa fa-users"></i>
+                      <span>Artisans</span>
+                      <span class="menu-arrow arrow_carrot-right"></span>
+                  </a>
+        <ul class="sub">
+          <?php
+          $categories = DB::query('SELECT * FROM categories ORDER BY id DESC');
+          foreach ($categories as $category) {
+            ?>
+            <li><a class="" href="category_artisan.php?category = <?php echo $category['id'] ?>"><?php echo $category['category'] ?></a></li>
+            <?php
+          }
+           ?>
         </ul>
       </li>
       <li class="sub-menu">
